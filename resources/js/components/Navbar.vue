c5f993f52eece6c8431dff2b99fa7bedab9d4a79
godgodwinter <kakadz.pro@gmail.com>
2022-01-04 09:59:06 +0700

    update some layout

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,4 +1,43 @@
 <template>
+
+<div>
+  <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav> -->
+
+
   <nav class="navbar navbar-expand-lg navbar-light bg-white">
     <div class="container">
       <router-link :to="{ name: user ? 'home' : 'welcome' }" class="navbar-brand">
@@ -9,25 +48,32 @@
         <span class="navbar-toggler-icon" />
       </button>
 
-      <div id="navbar" class="collapse navbar-collapse">
-        <ul class="navbar-nav">
+      <div id="navbar" class="collapse navbar-collapse">22
+        <!-- <ul class="navbar-nav">
           <locale-dropdown />
-          <!-- <li class="nav-item">
+          <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
-          </li> -->
-        </ul>
+          </li>
+        </ul> -->
 
         <ul class="navbar-nav ms-auto">
+
           <!-- Authenticated -->
           <template v-if="user">
-            <li class="nav-item">
+             <router-link :to="{ name: 'users' }" class="nav-link" active-class="active">
+                {{ $t('Users') }}
+              </router-link>
+              <router-link :to="{ name: 'kategori' }" class="nav-link" active-class="active">
+                {{ $t('kategori') }}
+              </router-link>
+            <!-- <li class="nav-item">
               <router-link :to="{ name: 'users' }" class="nav-link" active-class="active">
                 {{ $t('Users') }}
               </router-link>
               <router-link :to="{ name: 'kategori' }" class="nav-link" active-class="active">
                 {{ $t('kategori') }}
               </router-link>
-            </li>
+            </li> -->
           <li  class="nav-item dropdown">
             <a class="nav-link dropdown-toggle text-dark"
                href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
@@ -71,6 +117,7 @@
       </div>
     </div>
   </nav>
+</div>
 </template>
 
 <script>
@@ -110,6 +157,6 @@ export default {
 }
 
 .container {
-  max-width: 1100px;
+  max-width: 1600px;
 }
 </style>
